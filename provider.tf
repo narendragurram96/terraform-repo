96d6b0a2d7ea0e03a17428c9b29a4a4a9aa5912d
narendragurram96 <91769722+narendragurram96@users.noreply.github.com>
2022-10-13 12:47:30 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIAUJTE4YK3QT5ZXWEO"
-  secret_key = "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"
+  access_key = "AKIATJYRVMEGI7B4PVVQ"
+  secret_key = "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"
 }
 
 terraform {
