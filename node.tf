2118af906ee22c48550dfbd00b2c50d8bafac960
narendragurram96 <91769722+narendragurram96@users.noreply.github.com>
2022-10-13 16:09:08 +0530

    Update node.tf

--- node.tf
+++ node.tf
@@ -28,9 +28,9 @@ resource "aws_iam_role_policy_attachment" "nodes-AmazonEC2ContainerRegistryReadO
   role       = aws_iam_role.nodes.name
 }
 
-resource "aws_eks_node_group" "private-nodes" {
+resource "aws_eks_node_group" "private-node" {
   cluster_name    = aws_eks_cluster.demo.name
-  node_group_name = "private-nodes"
+  node_group_name = "private-node"
   node_role_arn   = aws_iam_role.nodes.arn
 
   subnet_ids = [
